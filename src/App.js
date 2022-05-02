8fa30113e4e88d5dfc5cfa4cc90df6b7c1caa09a
AshrayShetty <ashray.shetty@m.pegasusinfocorp.com>
2022-05-02 13:34:09 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, Modal, TextField, Container, Typography, Box } from "@mui/material";
 
-function App() {
+const App = () => {
+  const [searchText, setSearchText] = useState("");
+  const [meaning, setMeaning] = useState({
+    word: "",
+    definition: []
+  });
+  const [err, setErr] = useState(false);
+  const [errMessage, setErrMessage] = useState("");
+  const [showModal, setShowModal] = useState(false);
+
+  const handleChange = e => {
+    setSearchText(e.target.value);
+    setErr(false);
+  };
+
+  const retriveData = data => {
+    const definition = [];
+    data.meanings.forEach(ele => {
+      ele.definitions.forEach(element => {
+        definition.push(element.definition);
+      });
+    });
+    setMeaning({
+      word: data.word,
+      definition
+    });
+    setShowModal(true);
+  };
+
+  const handleClick = () => {
+    if (searchText.length) {
+      fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchText}`, {
+        method: "GET",
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(response => {
+        if (response.status === 404) {
+          setErr(true);
+          setErrMessage(`Could not find meaning of ${searchText}`);
+          return
+        } else {
+          return response.json();
+        }
+      }).then(response => {
+        retriveData(response[0]);
+      }).catch(error => {
+        console.log(error);
+      });
+    } else {
+      setErr(true);
+      setErrMessage("Please provide some input");
+    }
+  };
+
+  const style = {
+    position: 'absolute',
+    top: '10%',
+    left: '50%',
+    transform: 'translate(-50%, 10%)',
+    width: '70%',
+    maxWidth: '400px',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Container maxWidth="sm" style={{ textAlign: "center" }}>
+      <h1> Dictionary </h1>
+        <TextField
+          error={err}
+          id="standard-basic"
+          label="Search"
+          variant="standard"
+          helperText={err ? errMessage : ""}
+          onChange={handleChange}
+        />
+      <div style={{ margin: "10px" }}>
+        <Button variant="contained" onClick={handleClick}>Search</Button>
+      </div>
+      <Modal
+        open={showModal}
+        onClose={() => setShowModal(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        style={{ overflow: 'scroll' }}
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Meaning for word {meaning.word}
+          </Typography>
+          {meaning.definition.map((ele , index) => (
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              {index+1} : {ele}
+            </Typography>
+          ))}
+        </Box>
+      </Modal>
+    </Container>
+  )
+};
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, hasInputValue,  } from '@testing-library/react';
+import {waitFor} from "./waitFor"
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+jest.setTimeout(15000)
+describe('testing App component of postive and negative test cases', () => {
+
+  test('Check if the blank textfeild throws error', async () => {
+    render(<App />);
+    const button = screen.getByRole('button', { name: 'Search' });
+    await fireEvent.click(button)
+    screen.getByText('Please provide some input')
+  });
+
+  test('check if the error is thrown when the meaning is not found', async () => {
+    render(<App />);
+    const input = screen.getByLabelText('Search',{selector : 'input'})
+    const button = screen.getByRole('button', { name: 'Search' });
+    fireEvent.change(input,{target : {value : 'asf'}})
+    fireEvent.click(button)
+    await waitFor(()=>{
+      screen.getByText('Could not find meaning of asf')
+    })
+  })
+
+  test('check if the modal pops up on the correct meaning', async () => {
+    render(<App />);
+    const input = screen.getByLabelText('Search',{selector : 'input'})
+    const button = screen.getByRole('button', { name: 'Search' });
+    fireEvent.change(input,{target : {value : 'dog'}})
+    fireEvent.click(button)
+    await waitFor(()=>{
+      screen.getByText('Meaning for word dog')
+    })
+  })
+})
+
